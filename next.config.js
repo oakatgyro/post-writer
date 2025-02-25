1e36efe481d3bcb15de42532d4002347ccbadb9c
Oak At Gyro <chokigon1120@gmail.com>
2025-02-25 21:19:42 +0900

    Refactor import syntax for Contentlayer in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const withContentlayer = require("next-contentlayer");
+import  { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = { reactStrictMode: true, swcMinify: true };
 
-module.exports = withContentlayer(nextConfig);
+export default withContentlayer(nextConfig);
